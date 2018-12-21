c2d1e714d7a65081410c867d8b4b7069d25955cf
RockinDesigner <43171022+RockinDesigner@users.noreply.github.com>
2018-12-21 08:22:22 +0300

    PUSHKA

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.buttons a, .letters h2 a').each(function() {
+  $('.buttons a, .arrow-down a, .letters h2 a, .tab-panel h4 a').each(function() {
     $(this).on('click', function(e) {
       e.preventDefault();
       var scrollTo = $(this).attr('href');
@@ -10,3 +10,19 @@ $(function() {
     });
   });
 });
+
+var tabButtons = document.querySelectorAll(".text-tabs .tab-buttons button");
+var tabPanel = document.querySelectorAll(".text-tabs .tab-panel");
+
+function showPanel(panelIndex) {
+  tabButtons.forEach(function (node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+  })
+  tabButtons[panelIndex].style.color = "white";
+  tabPanel.forEach(function(node) {
+    node.style.display = "none";
+  });
+  tabPanel[panelIndex].style.display = "block";
+}
+showPanel(0);
